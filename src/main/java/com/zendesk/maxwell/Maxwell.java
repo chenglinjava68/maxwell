c3ab1fe1d44b5fdd988f8666f4aeba5c1e185980
Ben Osheroff <ben@zendesk.com>
2015-09-08 13:50:55 -0700

    handle SAVEPOINT, degrade unhandled SQL to a warning

---
+++ src/main/java/com/zendesk/maxwell/Maxwell.java
@@ -0,0 +1,81 @@
+package com.zendesk.maxwell;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.zendesk.maxwell.producer.AbstractProducer;
+import com.zendesk.maxwell.schema.Schema;
+import com.zendesk.maxwell.schema.SchemaCapturer;
+import com.zendesk.maxwell.schema.SchemaStore;
+import com.zendesk.maxwell.schema.ddl.SchemaSyncError;
+
+public class Maxwell {
+	private Schema schema;
+	private MaxwellConfig config;
+	private MaxwellContext context;
+	static final Logger LOGGER = LoggerFactory.getLogger(Maxwell.class);
+
+	private void initFirstRun(Connection connection) throws SQLException, IOException, SchemaSyncError {
+		LOGGER.info("Maxwell is capturing initial schema");
+		SchemaCapturer capturer = new SchemaCapturer(connection);
+		this.schema = capturer.capture();
+
+		BinlogPosition pos = BinlogPosition.capture(connection);
+		SchemaStore store = new SchemaStore(connection, this.context.getServerID(), this.schema, pos);
+		store.save();
+
+		this.context.setInitialPosition(pos);
+	}
+
+	private void run(String[] argv) throws Exception {
+		this.config = new MaxwellConfig(argv);
+
+		if ( this.config.log_level != null )
+			MaxwellLogging.setLevel(this.config.log_level);
+
+		this.context = new MaxwellContext(this.config);
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				context.terminate();
+			}
+		});
+
+		try ( Connection connection = this.context.getConnectionPool().getConnection() ) {
+			MaxwellMysqlStatus.ensureMysqlState(connection);
+			SchemaStore.ensureMaxwellSchema(connection);
+
+			if ( this.context.getInitialPosition() != null ) {
+				LOGGER.info("Maxwell is booting, starting at " + this.context.getInitialPosition());
+				SchemaStore store = SchemaStore.restore(connection, this.context.getServerID(), this.context.getInitialPosition());
+				this.schema = store.getSchema();
+			} else {
+				initFirstRun(connection);
+			}
+		} catch ( SQLException e ) {
+			LOGGER.error("Failed to connect to mysql server @ " + this.config.getConnectionURI());
+			LOGGER.error(e.getLocalizedMessage());
+			return;
+		}
+
+		AbstractProducer producer = this.context.getProducer();
+
+		MaxwellParser p = new MaxwellParser(this.schema, producer, this.context, this.context.getInitialPosition());
+		p.run();
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			new Maxwell().run(args);
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
