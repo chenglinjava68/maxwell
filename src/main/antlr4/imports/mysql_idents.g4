6bd0a642f5cf8bba9a580f95890f4eafd91f6b50
Ben Osheroff <ben@zendesk.com>
2015-09-17 10:37:33 -0700

    Maxwell must crash when its position thread stops.
    In the event of a master cutover, Maxwell would previously amble
    blithely along its way with a totally dead position-store thread until
    it called SchemaPosition#setSync() and crashed.
    
    Now we are much more aggressive at crashing when the position thread
    crashes -- the most common case for ZD is that the master it is pointing
    to is read-only.

---
+++ src/main/antlr4/imports/mysql_idents.g4
@@ -0,0 +1,30 @@
+grammar mysql_idents;
+
+import mysql_literal_tokens;
+
+name: ( id | tokens_available_for_names );
+id: ( IDENT | QUOTED_IDENT );
+literal: (INTEGER_LITERAL | STRING_LITERAL | FLOAT_LITERAL);
+string: (IDENT | STRING_LITERAL);
+integer: INTEGER_LITERAL;
+charset_name: (IDENT | STRING_LITERAL | QUOTED_IDENT);
+
+SQL_COMMENT: '/*' (.)*? '*/' -> skip;
+SQL_LINE_COMMENT: ('#' | '--') (~'\n')* ('\n' | EOF) -> skip;
+
+STRING_LITERAL: TICK ('\\\'' | '\'\'' | ~('\''))* TICK;
+
+FLOAT_LITERAL: DIGIT* '.' DIGIT+;
+
+INTEGER_LITERAL: DIGIT+;
+
+fragment TICK: '\'';
+
+fragment UNQUOTED_CHAR: [0-9a-zA-Z\u0080-\u00FF$_];
+IDENT: (UNQUOTED_CHAR)+;
+
+fragment QUOTED_CHAR: ~('/' | '\\' | '.' | '`');
+QUOTED_IDENT: '`' QUOTED_CHAR+? '`';
+
+fragment DIGIT: [0-9];
+WS  :   [ \t\n\r]+ -> skip ;
---
+++ src/test/java/com/zendesk/maxwell/MysqlIsolatedServer.java
@@ -0,0 +1,97 @@
+package com.zendesk.maxwell;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class MysqlIsolatedServer {
+	public static final Long SERVER_ID = 123123L;
+	private Connection connection; private String baseDir;
+	private int port;
+
+	static final Logger LOGGER = LoggerFactory.getLogger(MysqlIsolatedServer.class);
+	public void boot() throws IOException, SQLException {
+        final String dir = System.getProperty("user.dir");
+
+		ProcessBuilder pb = new ProcessBuilder(dir + "/src/test/mysql_isolated_server/bin/boot_isolated_mysql_server",
+												"--", "--binlog_format=row", "--innodb_flush_log_at_trx_commit=1", "--server_id=" + SERVER_ID);
+		Map<String, String> env = pb.environment();
+
+		LOGGER.debug("Booting server: " + StringUtils.join(pb.command(), " "));
+		env.put("PATH", env.get("PATH") + ":/opt/local/bin");
+
+		Process p = pb.start();
+		InputStream pStdout = p.getInputStream();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(pStdout));
+
+		ArrayList<String> mysqlOut = new ArrayList<>();
+
+		while ( true ) {
+			String s = reader.readLine();
+			if ( s == null || s.equals("UP.") )
+				break;
+			mysqlOut.add(s);
+		}
+
+		if ( mysqlOut.size() == 0 ) {
+			String errLine;
+			BufferedReader stderrReader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+			while ( (errLine = stderrReader.readLine()) != null )
+				System.out.println("mysql isolated error: " + errLine);
+		}
+
+
+		String[] tmpDirSplit = mysqlOut.get(0).split(": ");
+
+		this.baseDir = tmpDirSplit[tmpDirSplit.length - 1];
+
+		String[] portSplit = mysqlOut.get(1).split(": ");
+		String port = portSplit[portSplit.length - 1];
+		System.out.println("Booted mysql server on port: " + port);
+
+		this.port = Integer.valueOf(port);
+
+		this.connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:" + port + "/mysql", "root", "");
+		this.connection.createStatement().executeUpdate("GRANT REPLICATION SLAVE on *.* to 'maxwell'@'127.0.0.1' IDENTIFIED BY 'maxwell'");
+		this.connection.createStatement().executeUpdate("GRANT ALL on `maxwell`.* to 'maxwell'@'127.0.0.1'");
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void executeList(List<String> queries) throws SQLException {
+		for (String q: queries) {
+			if ( q.matches("^\\s*$") )
+				continue;
+
+			getConnection().createStatement().executeUpdate(q);
+		}
+	}
+
+	public void executeList(String[] schemaSQL) throws SQLException {
+		executeList(Arrays.asList(schemaSQL));
+	}
+
+	public String getBaseDir() {
+		return baseDir;
+	}
+
+	public int getPort() {
+		return port;
+	}
+}
