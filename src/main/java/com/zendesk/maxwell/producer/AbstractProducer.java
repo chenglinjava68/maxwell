85de4fc51eaec5a503747a9c116a7fc6de0cecba
Ben Osheroff <ben@zendesk.com>
2015-11-26 07:53:45 -0800

    SCHEMA is an alias for DATABASE

---
+++ src/main/java/com/zendesk/maxwell/producer/AbstractProducer.java
@@ -0,0 +1,15 @@
+package com.zendesk.maxwell.producer;
+
+import com.zendesk.maxwell.MaxwellAbstractRowsEvent;
+import com.zendesk.maxwell.MaxwellContext;
+
+public abstract class AbstractProducer {
+	protected final MaxwellContext context;
+
+	public AbstractProducer(MaxwellContext context) {
+		this.context = context;
+	}
+	abstract public void push(MaxwellAbstractRowsEvent e) throws Exception;
+
+	public void onComplete(MaxwellAbstractRowsEvent e) { }
+}
