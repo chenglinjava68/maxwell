90500102afc8cb9917d33693c0dfae56f8535155
Ben Osheroff <ben@zendesk.com>
2016-01-05 20:36:48 -0800

    Tweaks for separation of mysql schema from mysql replication
    - probe both connections early, so that if we can't connect to one of
      them we can message properly which connection failed.
    - make allowances for empty passwords; it is a valid configuration,
      annoying though it may be.
    - silence the ridiculously chatty ConnectionPool

---
+++ src/main/java/com/zendesk/maxwell/MaxwellConfig.java
@@ -0,0 +1,281 @@
+package com.zendesk.maxwell;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import joptsimple.BuiltinHelpFormatter;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.zendesk.maxwell.schema.SchemaStore;
+
+public class MaxwellConfig {
+	static final Logger LOGGER = LoggerFactory.getLogger(MaxwellConfig.class);
+	static final String DEFAULT_CONFIG_FILE = "config.properties";
+
+	public MaxwellMysqlConfig replicationMysql;
+
+	public MaxwellMysqlConfig maxwellMysql;
+
+	public String  includeDatabases, excludeDatabases, includeTables, excludeTables;
+	public final Properties kafkaProperties;
+	public String kafkaTopic;
+	public String producerType;
+	public String outputFile;
+	public String log_level;
+
+	public Integer maxSchemas;
+	public BinlogPosition initPosition;
+	public boolean replayMode;
+
+	public MaxwellConfig() { // argv is only null in tests
+		this.kafkaProperties = new Properties();
+		this.replayMode = false;
+		this.replicationMysql = new MaxwellMysqlConfig();
+		this.maxwellMysql = new MaxwellMysqlConfig();
+	}
+
+	public MaxwellConfig(String argv[]) {
+		this();
+		this.parse(argv);
+		this.setDefaults();
+	}
+
+	private OptionParser getOptionParser() {
+		OptionParser parser = new OptionParser();
+		parser.accepts( "config", "location of config file" ).withRequiredArg();
+		parser.accepts( "log_level", "log level, one of DEBUG|INFO|WARN|ERROR" ).withRequiredArg();
+		parser.accepts( "replication_host", "mysql host to treat as master (if using separate schema and replication servers)" ).withRequiredArg();
+		parser.accepts( "replication_user", "mysql username with replication privileges (if using separate schema and replication servers)" ).withRequiredArg();
+		parser.accepts( "output_file", "output file for 'file' producer" ).withRequiredArg();
+		parser.accepts( "replication_password", "mysql password for replication_user (if using separate schema and replication servers)" ).withOptionalArg();
+		parser.accepts( "replication_port", "port for mysql replication_host (if using separate schema and replication servers)" ).withRequiredArg();
+
+		parser.accepts( "host", "mysql host with write access to maxwell database" ).withRequiredArg();
+		parser.accepts( "user", "mysql username with write access to maxwell database on host" ).withRequiredArg();
+		parser.accepts( "password", "mysql password for user on host" ).withOptionalArg();
+		parser.accepts( "port", "mysql port for host" ).withRequiredArg();
+
+		parser.accepts( "producer", "producer type: stdout|file|kafka" ).withRequiredArg();
+
+		parser.accepts( "kafka.bootstrap.servers", "at least one kafka server, formatted as HOST:PORT[,HOST:PORT]" ).withRequiredArg();
+		parser.accepts( "kafka_topic", "optionally provide a topic name to push to. default: maxwell").withOptionalArg();
+
+		parser.accepts( "max_schemas", "how many old schema definitions maxwell should keep around.  default: 5").withOptionalArg();
+		parser.accepts( "init_position", "initial binlog position, given as BINLOG_FILE:POSITION").withRequiredArg();
+		parser.accepts( "replay", "replay mode, don't store any information to the server");
+
+		parser.accepts( "include_dbs", "include these databases, formatted as include_dbs=db1,db2").withOptionalArg();
+		parser.accepts( "exclude_dbs", "exclude these databases, formatted as exclude_dbs=db1,db2").withOptionalArg();
+		parser.accepts( "include_tables", "include these tables, formatted as include_tables=db1,db2").withOptionalArg();
+		parser.accepts( "exclude_tables", "exclude these tables, formatted as exclude_tables=tb1,tb2").withOptionalArg();
+		parser.accepts( "help", "display help").forHelp();
+		parser.formatHelpWith(new BuiltinHelpFormatter(160, 4));
+		return parser;
+	}
+
+	private String parseLogLevel(String level) {
+		level = level.toLowerCase();
+		if ( !( level.equals("debug") || level.equals("info") || level.equals("warn") || level.equals("error")))
+			usage("unknown log level: " + level);
+		return level;
+	}
+
+	private void parse(String [] argv) {
+		OptionSet options = getOptionParser().parse(argv);
+
+		if ( options.has("config") ) {
+			parseFile((String) options.valueOf("config"), true);
+		} else {
+			parseFile(DEFAULT_CONFIG_FILE, false);
+		}
+
+		if ( options.has("help") )
+			usage("Help for Maxwell:");
+
+		if ( options.has("log_level")) {
+			this.log_level = parseLogLevel((String) options.valueOf("log_level"));
+		}
+
+		this.maxwellMysql.parseOptions("", options);
+
+		this.replicationMysql.parseOptions("replication_", options);
+
+		if ( options.has("producer"))
+			this.producerType = (String) options.valueOf("producer");
+
+		if ( options.has("kafka.bootstrap.servers"))
+			this.kafkaProperties.setProperty("bootstrap.servers", (String) options.valueOf("kafka.bootstrap.servers"));
+
+		if ( options.has("kafka_topic"))
+			this.kafkaTopic = (String) options.valueOf("kafka_topic");
+
+		if ( options.has("output_file"))
+			this.outputFile = (String) options.valueOf("output_file");
+
+		if ( options.has("max_schemas"))
+			this.maxSchemas = Integer.valueOf((String)options.valueOf("max_schemas"));
+
+		if ( options.has("init_position")) {
+			String initPosition = (String) options.valueOf("init_position");
+			String[] initPositionSplit = initPosition.split(":");
+
+			if ( initPositionSplit.length != 2 )
+				usage("Invalid init_position: " + initPosition);
+
+			Long pos = 0L;
+			try {
+				pos = Long.valueOf(initPositionSplit[1]);
+			} catch ( NumberFormatException e ) {
+				usage("Invalid init_position: " + initPosition);
+			}
+
+			this.initPosition = new BinlogPosition(pos, initPositionSplit[0]);
+		}
+
+		if ( options.has("replay")) {
+			this.replayMode = true;
+		}
+
+		if ( options.has("include_dbs"))
+			this.includeDatabases = (String) options.valueOf("include_dbs");
+		
+		if ( options.has("exclude_dbs"))
+			this.excludeDatabases = (String) options.valueOf("exclude_dbs");
+
+		if ( options.has("include_tables"))
+			this.includeTables = (String) options.valueOf("include_tables");
+		
+		if ( options.has("exclude_tables"))
+			this.excludeTables = (String) options.valueOf("exclude_tables");
+	}
+
+	private Properties readPropertiesFile(String filename, Boolean abortOnMissing) {
+		Properties p = null;
+		try {
+			File file = new File(filename);
+			if ( !file.exists() ) {
+				if ( abortOnMissing ) {
+					System.err.println("Couldn't find config file: " + filename);
+					System.exit(1);
+				} else {
+					return null;
+				}
+			}
+
+			FileReader reader = new FileReader(file);
+			p = new Properties();
+			p.load(reader);
+		} catch ( IOException e ) {
+			System.err.println("Couldn't read config file: " + e);
+			System.exit(1);
+		}
+		return p;
+	}
+
+	private void parseFile(String filename, Boolean abortOnMissing) {
+		Properties p = readPropertiesFile(filename, abortOnMissing);
+
+		if ( p == null )
+			return;
+
+
+		this.maxwellMysql.host = p.getProperty("host", "127.0.0.1");
+		this.maxwellMysql.password = p.getProperty("password");
+		this.maxwellMysql.user     = p.getProperty("user");
+		this.maxwellMysql.port = Integer.valueOf(p.getProperty("port", "3306"));
+
+		this.replicationMysql.host = p.getProperty("replication_host");
+		this.replicationMysql.password = p.getProperty("replication_password");
+		this.replicationMysql.user      = p.getProperty("replication_user");
+		this.replicationMysql.port = Integer.valueOf(p.getProperty("replication_port", "3306"));
+
+		this.producerType    = p.getProperty("producer");
+		this.outputFile      = p.getProperty("output_file");
+		this.kafkaTopic      = p.getProperty("kafka_topic");
+		this.includeDatabases = p.getProperty("include_dbs");
+		this.excludeDatabases = p.getProperty("exclude_dbs");
+		this.includeTables = p.getProperty("include_tables");
+		this.excludeTables = p.getProperty("exclude_tables");
+
+		String maxSchemaString = p.getProperty("max_schemas");
+		if (maxSchemaString != null)
+			this.maxSchemas      = Integer.valueOf(maxSchemaString);
+
+		if ( p.containsKey("log_level") )
+			this.log_level = parseLogLevel(p.getProperty("log_level"));
+
+		for ( Enumeration<Object> e = p.keys(); e.hasMoreElements(); ) {
+			String k = (String) e.nextElement();
+			if ( k.startsWith("kafka.")) {
+				this.kafkaProperties.setProperty(k.replace("kafka.", ""), p.getProperty(k));
+			}
+		}
+
+	}
+
+	private void setDefaults() {
+		if ( this.producerType == null ) {
+			this.producerType = "stdout";
+		} else if ( this.producerType.equals("kafka")
+				&& !this.kafkaProperties.containsKey("bootstrap.servers")) {
+			usage("You must specify kafka.bootstrap.servers for the kafka producer!");
+		} else if ( this.producerType.equals("file")
+				&& this.outputFile == null) {
+			usage("please specify --output_file=FILE to use the file producer");
+		}
+
+		if ( this.maxwellMysql.port == null )
+			this.maxwellMysql.port = 3306;
+
+		if ( this.maxwellMysql.user == null) {
+			this.maxwellMysql.user = "maxwell";
+		}
+
+		if ( this.maxwellMysql.host == null ) {
+			LOGGER.warn("maxwell mysql host not specified, defaulting to localhost");
+			this.maxwellMysql.host = "localhost";
+		}
+
+		if ( this.replicationMysql.port == null )
+			this.replicationMysql.port = 3306;
+
+		if ( this.replicationMysql.host == null
+				|| this.replicationMysql.user == null ) {
+
+			if (this.replicationMysql.host != null
+					|| this.replicationMysql.user != null
+					|| this.replicationMysql.password != null) {
+				usage("Specified a replication option but missing one of the following options: replication_host, replication_user, replication_password.");
+			}
+			
+			this.replicationMysql = new MaxwellMysqlConfig(this.maxwellMysql.host,
+									this.maxwellMysql.port,
+									this.maxwellMysql.user,
+									this.maxwellMysql.password);
+		}
+
+		if ( this.maxSchemas != null )
+			SchemaStore.setMaxSchemas(this.maxSchemas);
+	}
+
+	private void usage(String string) {
+		System.err.println(string);
+		System.err.println();
+		try {
+			getOptionParser().printHelpOn(System.err);
+			System.exit(1);
+		} catch (IOException e) {
+		}
+	}
+
+	public Properties getKafkaProperties() {
+		return this.kafkaProperties;
+	}
+}
---
+++ src/test/java/com/zendesk/maxwell/AbstractMaxwellTest.java
@@ -0,0 +1,156 @@
+package com.zendesk.maxwell;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.zendesk.maxwell.schema.Schema;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import com.zendesk.maxwell.schema.SchemaCapturer;
+import com.zendesk.maxwell.schema.SchemaStore;
+
+public class AbstractMaxwellTest {
+	protected static MysqlIsolatedServer server;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		server = new MysqlIsolatedServer();
+		server.boot();
+
+		SchemaStore.ensureMaxwellSchema(server.getConnection());
+	}
+
+	@AfterClass
+	public static void teardownServer() {
+		server.shutDown();
+	}
+
+	public String getSQLDir() {
+		 final String dir = System.getProperty("user.dir");
+		 return dir + "/src/test/resources/sql/";
+	}
+
+
+	private void resetMaster() throws SQLException, IOException {
+		List<String> queries = new ArrayList<String>(Arrays.asList(
+				"CREATE DATABASE if not exists shard_2",
+				"DROP DATABASE if exists shard_1",
+				"CREATE DATABASE shard_1",
+				"USE shard_1"
+		));
+
+		for ( File file: new File(getSQLDir() + "/schema").listFiles()) {
+			if ( !file.getName().endsWith(".sql"))
+				continue;
+
+			byte[] sql = Files.readAllBytes(file.toPath());
+			String s = new String(sql);
+			if ( s != null ) {
+				queries.add(s);
+			}
+		}
+
+		queries.add("RESET MASTER");
+
+		server.executeList(queries);
+	}
+
+	private void generateBinlogEvents() throws IOException, SQLException {
+		Path p = Paths.get(getSQLDir() + "/rows/rows.sql");
+		List<String>sql = Files.readAllLines(p, Charset.forName("UTF-8"));
+
+		server.executeList(sql);
+	}
+
+	@Before
+	public void setupMysql() throws SQLException, IOException, InterruptedException {
+		resetMaster();
+		generateBinlogEvents();
+	}
+
+	protected MaxwellContext buildContext(int port, BinlogPosition p) {
+		MaxwellConfig config = new MaxwellConfig();
+
+		config.replicationMysql.host = "127.0.0.1";
+		config.replicationMysql.port = port;
+		config.replicationMysql.user = "maxwell";
+		config.replicationMysql.password = "maxwell";
+
+		config.maxwellMysql.host = "127.0.0.1";
+		config.maxwellMysql.port = port;
+		config.maxwellMysql.user = "maxwell";
+		config.maxwellMysql.password = "maxwell";
+
+		config.initPosition = p;
+
+		return new MaxwellContext(config);
+	}
+
+	protected MaxwellContext buildContext(BinlogPosition p) {
+		return buildContext(server.getPort(), p);
+	}
+
+	protected MaxwellContext buildContext() {
+		return buildContext(null);
+	}
+
+	protected List<RowMap>getRowsForSQL(MysqlIsolatedServer mysql, MaxwellFilter filter, String queries[], String before[]) throws Exception {
+		BinlogPosition start = BinlogPosition.capture(mysql.getConnection());
+		MaxwellContext context = buildContext(mysql.getPort(), null);
+		SchemaCapturer capturer = new SchemaCapturer(mysql.getConnection(), context.getCaseSensitivity());
+
+		if ( before != null ) {
+			mysql.executeList(Arrays.asList(before));
+		}
+
+
+		Schema initialSchema = capturer.capture();
+
+		mysql.executeList(Arrays.asList(queries));
+
+		BinlogPosition endPosition = BinlogPosition.capture(mysql.getConnection());
+
+		TestMaxwellReplicator p = new TestMaxwellReplicator(initialSchema,  null, context, start, endPosition);
+
+		p.setFilter(filter);
+
+
+		final ArrayList<RowMap> list = new ArrayList<>();
+
+		p.getEvents(new RowConsumer() {
+			@Override
+			void consume(RowMap r) {
+				if (!r.getDatabase().equals("maxwell")) {
+					list.add(r);
+				}
+			}
+		});
+
+		context.terminate();
+
+		return list;
+	}
+
+	protected List<RowMap>getRowsForSQL(MaxwellFilter filter, String queries[], String before[]) throws Exception {
+		return getRowsForSQL(server, filter, queries, before);
+	}
+
+	protected List<RowMap>getRowsForSQL(MaxwellFilter filter, String queries[]) throws Exception {
+		return getRowsForSQL(filter, queries, null);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+}
