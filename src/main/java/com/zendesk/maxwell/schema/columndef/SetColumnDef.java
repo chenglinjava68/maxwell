8998772ae6d74757508341b649d75b8bf2d34f01
Ben Osheroff <ben@zendesk.com>
2016-02-11 09:37:02 -0800

    Merge pull request #266 from zendesk/bootstrap_tweaks_3
    Bootstrap tweaks, part 3 of 3

---
+++ src/main/java/com/zendesk/maxwell/schema/columndef/SetColumnDef.java
@@ -0,0 +1,44 @@
+package com.zendesk.maxwell.schema.columndef;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.code.or.common.util.MySQLConstants;
+
+public class SetColumnDef extends ColumnDef {
+	public SetColumnDef(String tableName, String name, String type, int pos, String[] enumValues) {
+		super(tableName, name, type, pos);
+		this.enumValues = enumValues;
+	}
+
+	@Override
+	public boolean matchesMysqlType(int type) {
+		return type == MySQLConstants.TYPE_SET;
+	}
+
+	@Override
+	public String toSQL(Object value) {
+		return "'" + StringUtils.join(asList(value), "'") + "'";
+	}
+
+	@Override
+	public Object asJSON(Object value) {
+		return asList(value);
+	}
+
+	private ArrayList<String> asList(Object value) {
+		if ( value instanceof String ) {
+			return new ArrayList<>(Arrays.asList((( String ) value).split(",")));
+		}
+		ArrayList<String> values = new ArrayList<>();
+		long v = (Long) value;
+		for(int i = 0; i < enumValues.length; i++) {
+			if ( ((v >> i) & 1) == 1 ) {
+				values.add(enumValues[i]);
+			}
+		}
+		return values;
+	}
+}
