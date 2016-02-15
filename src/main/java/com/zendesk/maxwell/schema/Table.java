1172d883ade59f29e523b259aaf42a5bd5794d15
Ben Osheroff <ben@zendesk.com>
2016-02-14 22:41:12 -0800

    normalize database / table names in DDL classes
    will help us get into JSON in a consistant fashion.

---
+++ src/main/java/com/zendesk/maxwell/schema/Table.java
@@ -0,0 +1,214 @@
+package com.zendesk.maxwell.schema;
+
+import java.util.*;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.zendesk.maxwell.schema.columndef.ColumnDef;
+import com.zendesk.maxwell.schema.columndef.StringColumnDef;
+
+public class Table {
+	private final List<ColumnDef> columnList;
+	int pkIndex;
+	private String name;
+
+	private Database database;
+	private final String charset;
+	private List<String> pkColumnNames;
+	private HashMap<String, Integer> columnOffsetMap;
+
+	public Table(Database d, String name, String charset, List<ColumnDef> list, List<String> pks) {
+		this.database = d;
+		this.name = name;
+		this.charset = charset;
+		this.columnList = list;
+
+		if ( pks == null )
+			pks = new ArrayList<String>();
+
+		this.setPKList(pks);
+
+		renumberColumns();
+	}
+
+	public List<ColumnDef> getColumnList() {
+		return columnList;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	private void initColumnOffsetMap() {
+		if ( this.columnOffsetMap != null )
+			return;
+
+		this.columnOffsetMap = new HashMap<>();
+		int i = 0;
+
+		for(ColumnDef c : columnList) {
+			this.columnOffsetMap.put(c.getName(), i++);
+		}
+	}
+
+	public int findColumnIndex(String name) {
+		String lcName = name.toLowerCase();
+		initColumnOffsetMap();
+
+		if ( this.columnOffsetMap.containsKey(lcName) ) {
+			return this.columnOffsetMap.get(lcName);
+		} else {
+			return -1;
+		}
+	}
+
+	private ColumnDef findColumn(String name) {
+		String lcName = name.toLowerCase();
+
+		for (ColumnDef c : columnList )  {
+			if ( c.getName().equals(lcName) )
+				return c;
+		}
+
+		return null;
+	}
+
+
+	public int getPKIndex() {
+		return this.pkIndex;
+	}
+
+	public Database getDatabase() {
+		return database;
+	}
+
+	public Table copy() {
+		ArrayList<ColumnDef> list = new ArrayList<>();
+
+		for ( ColumnDef c : columnList ) {
+			list.add(c.copy());
+		}
+
+		return new Table(database, name, charset, list, pkColumnNames);
+	}
+
+	public void rename(String tableName) {
+		this.name = tableName;
+	}
+
+	private void diffColumnList(List<String> diffs, Table a, Table b, String nameA, String nameB) {
+		for ( ColumnDef column : a.getColumnList() ) {
+			ColumnDef other = b.findColumn(column.getName());
+			if ( other == null )
+				diffs.add(b.fullName() + " is missing column " + column.getName() + " in " + nameB);
+			else {
+                String colName = a.fullName() + ".`" + column.getName() + "` ";
+				if ( !column.getType().equals(other.getType()) ) {
+					diffs.add(colName + "has a type mismatch, "
+									  + column.getType()
+									  + " vs "
+									  + other.getType()
+									  + " in " + nameB);
+				} else if ( column.getPos() != other.getPos() ) {
+					diffs.add(colName + "has a position mismatch, "
+									  + column.getPos()
+									  + " vs "
+									  + other.getPos()
+									  + " in " + nameB);
+				} else if ( !Arrays.deepEquals(column.getEnumValues(), other.getEnumValues()) ) {
+					diffs.add(colName + "has an enum value mismatch, "
+									  + StringUtils.join(column.getEnumValues(), ",")
+									  + " vs "
+									  + StringUtils.join(other.getEnumValues(), ",")
+									  + " in " + nameB);
+
+				} else if ( !Objects.equals(column.getCharset(), other.getCharset()) ) {
+					diffs.add(colName + "has an charset mismatch, "
+						+ "'" + column.getCharset() + "'"
+						+ " vs "
+						+ "'" + other.getCharset() + "'"
+						+ " in " + nameB);
+
+				}
+			}
+		}
+	}
+
+	public String fullName() {
+		return "`" + this.database.getName() + "`." + this.name + "`";
+	}
+
+	public void diff(List<String> diffs, Table other, String nameA, String nameB) {
+		if ( !this.getCharset().equals(other.getCharset()) ) {
+			diffs.add(this.fullName() + " differs in charset: "
+					  + nameA + " is " + this.getCharset() + " but "
+					  + nameB + " is " + other.getCharset());
+		}
+
+		if ( !this.getPKString().equals(other.getPKString())) {
+			diffs.add(this.fullName() + " differs in PKs: "
+					  + nameA + " is " + this.getPKString() + " but "
+					  + nameB + " is " + other.getPKString());
+		}
+		diffColumnList(diffs, this, other, nameA, nameB);
+		diffColumnList(diffs, other, this, nameB, nameA);
+	}
+
+	private void renumberColumns() {
+		int i = 0 ;
+		for ( ColumnDef c : columnList ) {
+			c.setPos(i++);
+		}
+	}
+
+	public void setDefaultColumnCharsets() {
+		for ( ColumnDef c : columnList ) {
+			if ( c instanceof StringColumnDef ) {
+				((StringColumnDef) c).setDefaultCharset(this.getCharset());
+			}
+		}
+	}
+
+	public void addColumn(int index, ColumnDef definition) {
+		this.columnList.add(index, definition);
+		this.columnOffsetMap = null;
+		renumberColumns();
+	}
+
+	public void addColumn(ColumnDef defintion) {
+		addColumn(this.columnList.size(), defintion);
+	}
+
+	public void removeColumn(int idx) {
+		this.columnList.remove(idx);
+		this.columnOffsetMap = null;
+		renumberColumns();
+	}
+
+	public void setDatabase(Database database) {
+		this.database = database;
+	}
+
+	public String getCharset() {
+		if ( charset == null ) {
+			return this.database.getCharset();
+		} else {
+			return charset;
+		}
+	}
+
+	public List<String> getPKList() {
+		return this.pkColumnNames;
+	}
+
+	public String getPKString() {
+		if ( this.pkColumnNames != null )
+			return StringUtils.join(pkColumnNames.iterator(), ",");
+		else
+			return null;
+	}
+
+	public void setPKList(List<String> pkColumnNames) {
+		this.pkColumnNames = pkColumnNames;
+	}
+}
---
+++ src/main/java/com/zendesk/maxwell/schema/columndef/ColumnDef.java
@@ -0,0 +1,201 @@
+package com.zendesk.maxwell.schema.columndef;
+
+public abstract class ColumnDef {
+	protected final String tableName;
+	protected final String name;
+	protected final String type;
+	protected String[] enumValues;
+	private int pos;
+	public boolean signed;
+	public String charset;
+
+	public ColumnDef(String tableName, String name, String type, int pos) {
+		this.tableName = tableName;
+		this.name = name.toLowerCase();
+		this.type = type;
+		this.pos = pos;
+		this.signed = false;
+	}
+
+	public abstract boolean matchesMysqlType(int type);
+	public abstract String toSQL(Object value);
+
+	public Object asJSON(Object value) {
+		return value;
+	}
+
+	public ColumnDef copy() {
+		return build(this.tableName, this.name, this.charset, this.type, this.pos, this.signed, this.enumValues);
+	}
+
+	public static ColumnDef build(String tableName, String name, String charset, String type, int pos, boolean signed, String enumValues[]) {
+		switch(type) {
+		case "tinyint":
+		case "smallint":
+		case "mediumint":
+		case "int":
+			return new IntColumnDef(tableName, name, type, pos, signed);
+		case "bigint":
+			return new BigIntColumnDef(tableName, name, type, pos, signed);
+		case "tinytext":
+		case "text":
+		case "mediumtext":
+		case "longtext":
+		case "varchar":
+		case "char":
+			return new StringColumnDef(tableName, name, type, pos, charset);
+		case "tinyblob":
+		case "blob":
+		case "mediumblob":
+		case "longblob":
+		case "binary":
+		case "varbinary":
+			return new StringColumnDef(tableName, name, type, pos, "binary");
+		case "geometry":
+		case "geometrycollection":
+		case "linestring":
+		case "multilinestring":
+		case "multipoint":
+		case "multipolygon":
+		case "polygon":
+		case "point":
+			return new GeometryColumnDef(tableName, name, type, pos);
+		case "float":
+		case "double":
+			return new FloatColumnDef(tableName, name, type, pos);
+		case "decimal":
+			return new DecimalColumnDef(tableName, name, type, pos);
+		case "date":
+			return new DateColumnDef(tableName, name, type, pos);
+		case "datetime":
+		case "timestamp":
+			return new DateTimeColumnDef(tableName, name, type, pos);
+		case "year":
+			return new YearColumnDef(tableName, name, type, pos);
+		case "time":
+			return new TimeColumnDef(tableName, name, type, pos);
+		case "enum":
+			return new EnumColumnDef(tableName, name, type, pos, enumValues);
+		case "set":
+			return new SetColumnDef(tableName, name, type, pos, enumValues);
+		case "bit":
+			return new BitColumnDef(tableName, name, type, pos);
+		default:
+			throw new IllegalArgumentException("unsupported column type " + type);
+		}
+	}
+
+	static private String charToByteType(String type) {
+		switch (type) {
+			case "char":
+			case "character":
+				return "binary";
+			case "varchar":
+			case "varying":
+				return "varbinary";
+			case "tinytext":
+				return "tinyblob";
+			case "text":
+				return "blob";
+			case "mediumtext":
+				return "mediumblob";
+			case "longtext":
+				return "longblob";
+			case "long":
+				return "mediumblob";
+			default:
+				throw new RuntimeException("Unknown type with BYTE flag: " + type);
+		}
+	}
+
+	static public String unalias_type(String type, boolean longStringFlag, Long columnLength, boolean byteFlagToStringColumn) {
+		if ( byteFlagToStringColumn )
+			type = charToByteType(type);
+
+		if ( longStringFlag ) {
+			switch (type) {
+				case "varchar":
+					return "mediumtext";
+				case "varbinary":
+					return "mediumblob";
+				case "binary":
+					return "mediumtext";
+			}
+		}
+
+		switch(type) {
+			case "character":
+			case "nchar":
+				return "char";
+			case "text":
+			case "blob":
+				if ( columnLength == null )
+					return type;
+
+				if ( columnLength < (1 << 8) )
+					return "tiny" + type;
+				else if ( columnLength < ( 1 << 16) )
+					return type;
+				else if ( columnLength < ( 1 << 24) )
+					return "medium" + type;
+				else
+					return "long" + type;
+			case "nvarchar":
+			case "varying":
+				return "varchar";
+			case "bool":
+			case "boolean":
+			case "int1":
+				return "tinyint";
+			case "int2":
+				return "smallint";
+			case "int3":
+				return "mediumint";
+			case "int4":
+			case "integer":
+				return "int";
+			case "int8":
+				return "bigint";
+			case "real":
+			case "numeric":
+				return "double";
+			case "long":
+				return "mediumtext";
+			default:
+				return type;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public int getPos() {
+		return pos;
+	}
+
+	public void setPos(int i) {
+		this.pos = i;
+	}
+
+	public String getCharset() {
+		return this.charset;
+	}
+
+	public boolean getSigned() {
+		return this.signed;
+	}
+
+	public String[] getEnumValues() {
+		return enumValues;
+	}
+
+}
