c0a32fbca05e280f08c9de82b3001bee7b0e34f0
Ben Osheroff <ben@zendesk.com>
2016-12-13 15:43:52 -0800

    remove stray comment

---
+++ src/main/java/com/zendesk/maxwell/recovery/RecoveryInfo.java
@@ -0,0 +1,17 @@
+package com.zendesk.maxwell.recovery;
+
+import com.zendesk.maxwell.replication.BinlogPosition;
+
+public class RecoveryInfo {
+	public BinlogPosition position;
+	public Long heartbeat;
+	public Long serverID;
+	public String clientID;
+
+	public RecoveryInfo(BinlogPosition position, Long heartbeat, Long serverID, String clientID) {
+		this.position = position;
+		this.heartbeat = heartbeat;
+		this.serverID = serverID;
+		this.clientID = clientID;
+	}
+}
