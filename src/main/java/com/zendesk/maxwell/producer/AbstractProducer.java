fae021c113c8c4399dcb11ed6e6585a2311d9354
Ben Osheroff <ben@zendesk.com>
2016-12-26 22:31:13 -0800

    force a heartbeat after we our post-replicator things.  more recovery-test stabilization.

---
+++ src/main/java/com/zendesk/maxwell/producer/AbstractProducer.java
@@ -0,0 +1,23 @@
+package com.zendesk.maxwell.producer;
+
+import java.sql.SQLException;
+
+import com.zendesk.maxwell.replication.BinlogPosition;
+import com.zendesk.maxwell.MaxwellContext;
+import com.zendesk.maxwell.row.RowMap;
+
+public abstract class AbstractProducer {
+	protected final MaxwellContext context;
+	protected final MaxwellOutputConfig outputConfig;
+
+	public AbstractProducer(MaxwellContext context) {
+		this.context = context;
+		this.outputConfig = context.getConfig().outputConfig;
+	}
+
+	abstract public void push(RowMap r) throws Exception;
+
+	public void writePosition(BinlogPosition p) throws SQLException {
+		this.context.setPosition(p);
+	}
+}
