9bab4f2c559dc8d8b262fa255d9a7928c2021b0d
Ben Osheroff <ben@zendesk.com>
2017-01-03 00:57:51 -0800

    fixes for kinesis command line args

---
+++ src/main/java/com/zendesk/maxwell/producer/MaxwellKafkaProducer.java
@@ -0,0 +1,124 @@
+package com.zendesk.maxwell.producer;
+
+import com.zendesk.maxwell.replication.BinlogPosition;
+import com.zendesk.maxwell.schema.ddl.DDLMap;
+import com.zendesk.maxwell.MaxwellContext;
+import com.zendesk.maxwell.row.RowMap;
+import com.zendesk.maxwell.row.RowMap.KeyFormat;
+import com.zendesk.maxwell.producer.partitioners.MaxwellKafkaPartitioner;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.errors.RecordTooLargeException;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+class KafkaCallback implements Callback {
+	public static final Logger LOGGER = LoggerFactory.getLogger(MaxwellKafkaProducer.class);
+	private final AbstractAsyncProducer.CallbackCompleter cc;
+	private final BinlogPosition position;
+	private final String json;
+	private final String key;
+
+	public KafkaCallback(AbstractAsyncProducer.CallbackCompleter cc, BinlogPosition position, String key, String json) {
+		this.cc = cc;
+		this.position = position;
+		this.key = key;
+		this.json = json;
+	}
+
+	@Override
+	public void onCompletion(RecordMetadata md, Exception e) {
+		if ( e != null ) {
+			LOGGER.error(e.getClass().getSimpleName() + " @ " + position + " -- " + key);
+			LOGGER.error(e.getLocalizedMessage());
+			if ( e instanceof RecordTooLargeException ) {
+				LOGGER.error("Considering raising max.request.size broker-side.");
+			}
+		} else {
+			if ( LOGGER.isDebugEnabled()) {
+				LOGGER.debug("->  key:" + key + ", partition:" +md.partition() + ", offset:" + md.offset());
+				LOGGER.debug("   " + this.json);
+				LOGGER.debug("   " + position);
+				LOGGER.debug("");
+			}
+		}
+		cc.markCompleted();
+	}
+}
+
+public class MaxwellKafkaProducer extends AbstractAsyncProducer {
+	static final Logger LOGGER = LoggerFactory.getLogger(MaxwellKafkaProducer.class);
+
+	private final KafkaProducer<String, String> kafka;
+	private String topic;
+	private final String ddlTopic;
+	private final MaxwellKafkaPartitioner partitioner;
+	private final MaxwellKafkaPartitioner ddlPartitioner;
+	private final KeyFormat keyFormat;
+
+	public MaxwellKafkaProducer(MaxwellContext context, Properties kafkaProperties, String kafkaTopic) {
+		super(context);
+
+		this.topic = kafkaTopic;
+		if ( this.topic == null ) {
+			this.topic = "maxwell";
+		}
+
+		this.kafka = new KafkaProducer<>(kafkaProperties, new StringSerializer(), new StringSerializer());
+
+		String hash = context.getConfig().kafkaPartitionHash;
+		String partitionKey = context.getConfig().producerPartitionKey;
+		String partitionColumns = context.getConfig().producerPartitionColumns;
+		String partitionFallback = context.getConfig().producerPartitionFallback;
+		this.partitioner = new MaxwellKafkaPartitioner(hash, partitionKey, partitionColumns, partitionFallback);
+		this.ddlPartitioner = new MaxwellKafkaPartitioner(hash, "database", null,"database");
+		this.ddlTopic =  context.getConfig().ddlKafkaTopic;
+
+		if ( context.getConfig().kafkaKeyFormat.equals("hash") )
+			keyFormat = KeyFormat.HASH;
+		else
+			keyFormat = KeyFormat.ARRAY;
+	}
+
+	private Integer getNumPartitions(String topic) {
+		try {
+			return this.kafka.partitionsFor(topic).size(); //returns 1 for new topics
+		} catch (KafkaException e) {
+			LOGGER.error("Topic '" + topic + "' name does not exist. Exception: " + e.getLocalizedMessage());
+			throw e;
+		}
+	}
+
+	private String generateTopic(String topic, RowMap r){
+		return topic.replaceAll("%\\{database\\}", r.getDatabase()).replaceAll("%\\{table\\}", r.getTable());
+	}
+
+	@Override
+	public void sendAsync(RowMap r, AbstractAsyncProducer.CallbackCompleter cc) throws Exception {
+		String key = r.pkToJson(keyFormat);
+		String value = r.toJSON(outputConfig);
+
+		ProducerRecord<String, String> record;
+		if (r instanceof DDLMap) {
+			record = new ProducerRecord<>(this.ddlTopic, this.ddlPartitioner.kafkaPartition(r, getNumPartitions(this.ddlTopic)), key, value);
+		} else {
+			String topic = generateTopic(this.topic, r);
+			record = new ProducerRecord<>(topic, this.partitioner.kafkaPartition(r, getNumPartitions(topic)), key, value);
+		}
+
+		/* if debug logging isn't enabled, release the reference to `value`, which can ease memory pressure somewhat */
+		if ( !KafkaCallback.LOGGER.isDebugEnabled() )
+			value = null;
+
+		KafkaCallback callback = new KafkaCallback(cc, r.getPosition(), key, value);
+
+		kafka.send(record, callback);
+	}
+}
