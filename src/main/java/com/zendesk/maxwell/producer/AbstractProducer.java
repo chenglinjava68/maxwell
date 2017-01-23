51de04eb4a305bbbf95a99229bebdb39e88e3038
Ben Osheroff <ben@zendesk.com>
2017-01-23 13:23:11 -0800

    CR: rename thread, Linked -> Array BlockingQueue

---
+++ src/main/java/com/zendesk/maxwell/producer/AbstractProducer.java
@@ -0,0 +1,17 @@
+package com.zendesk.maxwell.producer;
+
+import com.zendesk.maxwell.replication.BinlogPosition;
+import com.zendesk.maxwell.MaxwellContext;
+import com.zendesk.maxwell.row.RowMap;
+
+public abstract class AbstractProducer {
+	protected final MaxwellContext context;
+	protected final MaxwellOutputConfig outputConfig;
+
+	public AbstractProducer(MaxwellContext context) {
+		this.context = context;
+		this.outputConfig = context.getConfig().outputConfig;
+	}
+
+	abstract public void push(RowMap r) throws Exception;
+}
