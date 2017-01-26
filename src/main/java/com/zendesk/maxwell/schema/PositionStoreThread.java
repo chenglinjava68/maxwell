403e3e4e6e887999c4d984d4e11ad5809b757a2d
Ben Osheroff <ben@zendesk.com>
2017-01-26 15:18:35 -0800

    clear table-id cache on binlog rotate
    binlog rotate serves as a good enough proxy for server-restart, which is
    when table_map_ids will be reused.  See
    https://github.com/zendesk/maxwell/issues/546.

---
+++ src/main/java/com/zendesk/maxwell/schema/PositionStoreThread.java
@@ -0,0 +1,110 @@
+package com.zendesk.maxwell.schema;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+import java.util.concurrent.TimeoutException;
+
+import com.zendesk.maxwell.replication.BinlogPosition;
+import com.zendesk.maxwell.util.RunLoopProcess;
+
+public class PositionStoreThread extends RunLoopProcess implements Runnable {
+	static final Logger LOGGER = LoggerFactory.getLogger(PositionStoreThread.class);
+	private BinlogPosition position; // in memory position
+	private BinlogPosition storedPosition; // position as flushed to storage
+	private final MysqlPositionStore store;
+	private Exception exception;
+	private Thread thread;
+
+	public PositionStoreThread(MysqlPositionStore store) {
+		this.store = store;
+	}
+
+	public void start() {
+		this.thread = new Thread(this, "Position Flush Thread");
+		thread.start();
+	}
+
+	@Override
+	public void run() {
+		try {
+			runLoop();
+		} catch ( Exception e ) {
+			LOGGER.error("Hit " + e.getClass().getName() + " exception in MysqlPositionStore thread.");
+			this.exception = e;
+		}
+	}
+
+
+	public void stopLoop() throws TimeoutException {
+		this.requestStop();
+		thread.interrupt();
+		super.stopLoop();
+	}
+
+	@Override
+	protected void beforeStop() {
+		if ( exception == null ) {
+			LOGGER.info("Storing final position: " + position);
+			try {
+				store.set(position);
+			} catch ( Exception e ) {
+				LOGGER.error("error storing final position: " + e);
+			}
+		}
+	}
+
+	public void heartbeat() throws Exception {
+		store.heartbeat();
+	}
+
+	private boolean shouldHeartbeat(BinlogPosition heartbeatPosition, BinlogPosition currentPosition) {
+		if ( currentPosition == null )
+			return true;
+		if ( heartbeatPosition == null )
+			return true;
+		if ( !heartbeatPosition.getFile().equals(currentPosition.getFile()) )
+			return true;
+		if ( currentPosition.getOffset() - heartbeatPosition.getOffset() > 1000 ) {
+			return true;
+		}
+		return false;
+	}
+
+	BinlogPosition lastHeartbeatPosition = null; // last position we sent a heartbeat to
+	public void work() throws Exception {
+		BinlogPosition newPosition = position;
+
+		if ( newPosition != null && newPosition.newerThan(storedPosition) ) {
+			store.set(newPosition);
+			storedPosition = newPosition;
+		}
+
+		try { Thread.sleep(1000); } catch (InterruptedException e) { }
+
+		if ( shouldHeartbeat(lastHeartbeatPosition, position) )  {
+			store.heartbeat();
+			lastHeartbeatPosition = position;
+		}
+	}
+
+	public synchronized void setPosition(BinlogPosition p) {
+		if ( position == null || p.newerThan(position) )
+			position = p;
+	}
+
+	public synchronized BinlogPosition getPosition() throws SQLException {
+		if ( position != null )
+			return position;
+
+		position = store.get();
+
+		return position;
+	}
+
+	public Exception getException() {
+		return this.exception;
+	}
+}
+
