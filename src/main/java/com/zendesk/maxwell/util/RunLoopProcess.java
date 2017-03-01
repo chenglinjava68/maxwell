1f264b2903fef1a833523446e22826dfa1719f43
Andreas Scheja <asc@sinceat.net>
2017-03-01 21:00:05 +0100

    store enum values as json

---
+++ src/main/java/com/zendesk/maxwell/util/RunLoopProcess.java
@@ -0,0 +1,43 @@
+package com.zendesk.maxwell.util;
+
+import java.util.concurrent.TimeoutException;
+
+abstract public class RunLoopProcess implements StoppableTask {
+	protected volatile StoppableTaskState taskState;
+	private Thread thread;
+
+	public RunLoopProcess() {
+	}
+
+	public void requestStop() {
+		this.taskState.requestStop();
+	}
+
+	public void awaitStop(Long timeout) throws TimeoutException {
+		this.taskState.awaitStop(thread, timeout);
+	}
+
+	public boolean runLoop() throws Exception {
+		if ( this.taskState != null )
+			return false;
+
+		this.taskState = new StoppableTaskState(this.getClass().getName());
+		this.thread = Thread.currentThread();
+		this.beforeStart();
+
+		try {
+			while (this.taskState.isRunning()) {
+				work();
+			}
+		} finally {
+			this.beforeStop();
+			this.taskState.stopped();
+		}
+
+		return true;
+	}
+
+	protected abstract void work() throws Exception;
+	protected void beforeStart() throws Exception { }
+	protected void beforeStop() throws Exception { }
+}
