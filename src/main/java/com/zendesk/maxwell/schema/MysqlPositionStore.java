1d97f213ac82ed458dccb6cd2d5f01f9804bd90f
Tim Cuthbertson <tim@gfxmonk.net>
2017-03-07 15:39:54 +1100

    DRY-up some test code

---
+++ src/main/java/com/zendesk/maxwell/schema/MysqlPositionStore.java
@@ -0,0 +1,211 @@
+package com.zendesk.maxwell.schema;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+
+import com.zendesk.maxwell.recovery.RecoveryInfo;
+
+import com.zendesk.maxwell.replication.BinlogPosition;
+import com.zendesk.maxwell.errors.DuplicateProcessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import snaq.db.ConnectionPool;
+
+public class MysqlPositionStore {
+	static final Logger LOGGER = LoggerFactory.getLogger(MysqlPositionStore.class);
+	private static final Long DEFAULT_GTID_SERVER_ID = new Long(0);
+	private final Long serverID;
+	private String clientID;
+	private final boolean gtidMode;
+	private final ConnectionPool connectionPool;
+
+	public MysqlPositionStore(ConnectionPool pool, Long serverID, String clientID, boolean gtidMode) {
+		this.connectionPool = pool;
+		this.clientID = clientID;
+		this.gtidMode = gtidMode;
+		if (gtidMode) {
+			// we don't use server id for position store in gtid mode
+			this.serverID = DEFAULT_GTID_SERVER_ID;
+		} else {
+			this.serverID = serverID;
+		}
+	}
+
+	public void set(BinlogPosition newPosition) throws SQLException {
+		if ( newPosition == null )
+			return;
+
+		Long heartbeat = newPosition.getHeartbeat();
+		String lastHeartbeatSQL = heartbeat == null ? "" : "last_heartbeat_read = " + heartbeat + ", ";
+
+		String sql = "INSERT INTO `positions` set "
+				+ "server_id = ?, "
+				+ "gtid_set = ?, "
+				+ "binlog_file = ?, "
+				+ "binlog_position = ?, "
+				+ lastHeartbeatSQL
+				+ "client_id = ? "
+				+ "ON DUPLICATE KEY UPDATE "
+				+ lastHeartbeatSQL
+				+ "gtid_set = ?, binlog_file = ?, binlog_position=?";
+
+		try( Connection c = connectionPool.getConnection() ){
+			PreparedStatement s = c.prepareStatement(sql);
+
+			LOGGER.debug("Writing binlog position to " + c.getCatalog() + ".positions: " + newPosition + ", last heartbeat read: " + heartbeat);
+			s.setLong(1, serverID);
+			s.setString(2, newPosition.getGtidSetStr());
+			s.setString(3, newPosition.getFile());
+			s.setLong(4, newPosition.getOffset());
+			s.setString(5, clientID);
+			s.setString(6, newPosition.getGtidSetStr());
+			s.setString(7, newPosition.getFile());
+			s.setLong(8, newPosition.getOffset());
+
+			s.execute();
+		}
+	}
+
+	public synchronized void heartbeat() throws Exception {
+		try ( Connection c = connectionPool.getConnection() ) {
+			heartbeat(c);
+		}
+	}
+
+	/*
+	 * the heartbeat system performs two functions:
+	 * 1 - it leaves pointers in the binlog in order to facilitate master recovery
+	 * 2 - it detects duplicate maxwell processes configured with the same client_id, aborting if we detect a dupe.
+	 */
+
+	private Long lastHeartbeat = null;
+
+	private Long insertHeartbeat(Connection c, Long thisHeartbeat) throws SQLException, DuplicateProcessException {
+		String heartbeatInsert = "insert into `heartbeats` set `heartbeat` = ?, `server_id` = ?, `client_id` = ?";
+
+		PreparedStatement s = c.prepareStatement(heartbeatInsert);
+		s.setLong(1, thisHeartbeat);
+		s.setLong(2, serverID);
+		s.setString(3, clientID);
+
+		try {
+			s.execute();
+			return thisHeartbeat;
+		} catch ( MySQLIntegrityConstraintViolationException e ) {
+			throw new DuplicateProcessException("Found heartbeat row for client,position while trying to insert.  Is another maxwell running?");
+		}
+	}
+
+	private void heartbeat(Connection c) throws SQLException, DuplicateProcessException, InterruptedException {
+		Long thisHeartbeat = System.currentTimeMillis();
+
+		if ( lastHeartbeat == null ) {
+			PreparedStatement s = c.prepareStatement("SELECT `heartbeat` from `heartbeats` where server_id = ? and client_id = ?");
+			s.setLong(1, serverID);
+			s.setString(2, clientID);
+
+			ResultSet rs = s.executeQuery();
+			if ( !rs.next() ) {
+				insertHeartbeat(c, thisHeartbeat);
+				lastHeartbeat = thisHeartbeat;
+				return;
+			} else {
+				lastHeartbeat = rs.getLong("heartbeat");
+			}
+		}
+
+		String heartbeatUpdate = "update `heartbeats` set `heartbeat` = ? where `server_id` = ? and `client_id` = ? and `heartbeat` = ?";
+
+		PreparedStatement s = c.prepareStatement(heartbeatUpdate);
+		s.setLong(1, thisHeartbeat);
+		s.setLong(2, serverID);
+		s.setString(3, clientID);
+		s.setLong(4, lastHeartbeat);
+
+		LOGGER.debug("writing heartbeat: " + thisHeartbeat + " (last heartbeat written: " + lastHeartbeat + ")");
+		int nRows = s.executeUpdate();
+		if ( nRows != 1 ) {
+			String msg = String.format(
+				"Expected a heartbeat value of %d but didn't find it.  Is another Maxwell process running with the same client_id?",
+				lastHeartbeat
+			);
+
+			throw new DuplicateProcessException(msg);
+		}
+
+		lastHeartbeat = thisHeartbeat;
+	}
+
+	public BinlogPosition get() throws SQLException {
+		try ( Connection c = connectionPool.getConnection() ) {
+			PreparedStatement s = c.prepareStatement("SELECT * from `positions` where server_id = ? and client_id = ?");
+			s.setLong(1, serverID);
+			s.setString(2, clientID);
+
+			ResultSet rs = s.executeQuery();
+			if ( !rs.next() )
+				return null;
+
+			String gtid = gtidMode ? rs.getString("gtid_set") : null;
+			return new BinlogPosition(gtid, null,
+				rs.getLong("binlog_position"), rs.getString("binlog_file"), null);
+		}
+	}
+
+	/**
+	 * grabs a position from a different server_id
+	 */
+
+	public RecoveryInfo getRecoveryInfo() throws SQLException {
+		try ( Connection c = connectionPool.getConnection() ) {
+			return getRecoveryInfo(c);
+		}
+	}
+
+	private RecoveryInfo getRecoveryInfo(Connection c) throws SQLException {
+		RecoveryInfo info = null;
+
+		PreparedStatement s = c.prepareStatement("SELECT * from `positions` where client_id = ?");
+		s.setString(1, clientID);
+		ResultSet rs = s.executeQuery();
+
+
+		while ( rs.next() ) {
+			Long server_id = rs.getLong("server_id");
+			String gtid = gtidMode ? rs.getString("gtid_set") : null;
+			BinlogPosition position = BinlogPosition.at(gtid,
+				rs.getLong("binlog_position"), rs.getString("binlog_file"));
+			Long last_heartbeat_read = rs.getLong("last_heartbeat_read");
+
+			if ( rs.wasNull() ) {
+				LOGGER.warn("master recovery is ignorning position with NULL heartbeat");
+			} else if ( info != null ) {
+				LOGGER.error("found multiple binlog positions for cluster.  Not attempting position recovery.");
+				LOGGER.error("found a row for server_id: " + info.serverID);
+				LOGGER.error("also found a row for server_id: " + server_id);
+				return null;
+			} else {
+				info = new RecoveryInfo(position, last_heartbeat_read, server_id, clientID);
+			}
+		}
+		return info;
+	}
+
+	public int delete(Long serverID, String clientID, BinlogPosition position) throws SQLException {
+		try ( Connection c = connectionPool.getConnection()) {
+			PreparedStatement s = c.prepareStatement(
+				"DELETE from `positions` where server_id = ? and client_id = ? and binlog_file = ? and binlog_position = ?"
+			);
+			s.setLong(1, serverID);
+			s.setString(2, clientID);
+			s.setString(3, position.getFile());
+			s.setLong(4, position.getOffset());
+			s.execute();
+			return s.getUpdateCount();
+		}
+	}
+}
---
+++ src/test/java/com/zendesk/maxwell/schema/MysqlPositionStoreTest.java
@@ -0,0 +1,67 @@
+package com.zendesk.maxwell.schema;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.*;
+import com.zendesk.maxwell.*;
+import java.sql.ResultSet;
+
+import com.zendesk.maxwell.replication.BinlogPosition;
+import com.zendesk.maxwell.errors.DuplicateProcessException;
+import org.junit.Test;
+
+public class MysqlPositionStoreTest extends MaxwellTestWithIsolatedServer {
+	private MysqlPositionStore buildStore() throws Exception {
+		MaxwellContext context = buildContext();
+		return new MysqlPositionStore(context.getMaxwellConnectionPool(), context.getServerID(), "maxwell", MaxwellTestSupport.inGtidMode());
+	}
+
+	@Test
+	public void testSetBinlogPosition() throws Exception {
+		MysqlPositionStore store = buildStore();
+		if (MaxwellTestSupport.inGtidMode()) {
+			String gtid = "123:1-100";
+			store.set(new BinlogPosition(gtid, null, 12345, "foo", null));
+			assertThat(buildStore().get(), is(new BinlogPosition(gtid, null, 12345, "foo", null)));
+		} else {
+			store.set(new BinlogPosition(12345, "foo"));
+			assertThat(buildStore().get(), is(new BinlogPosition(12345, "foo")));
+		}
+	}
+
+	@Test
+	public void testHeartbeat() throws Exception {
+		MysqlPositionStore store = buildStore();
+		store.set(new BinlogPosition(12345, "foo"));
+
+		Long preHeartbeat = System.currentTimeMillis();
+		store.heartbeat();
+
+		ResultSet rs = server.getConnection().createStatement().executeQuery("select * from maxwell.heartbeats");
+		rs.next();
+
+		assertThat(rs.getLong("heartbeat") >= preHeartbeat, is(true));
+
+	}
+
+	@Test
+	public void testHeartbeatDuplicate() throws Exception {
+		MysqlPositionStore store = buildStore();
+		store.set(new BinlogPosition(12345, "foo"));
+
+		store.heartbeat();
+		buildStore().heartbeat();
+
+
+		Exception exception = null;
+
+		try {
+			store.heartbeat();
+		} catch (DuplicateProcessException d) {
+			exception = d;
+		}
+
+		assertThat(exception, is(not(nullValue())));
+	}
+}
